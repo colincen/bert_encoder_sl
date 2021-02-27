b7f974511d2d4022082c11c7b3a0342e6d980848
colin <colin2017@qq.com>
2021-02-27 13:41:49 +0800

    change bash

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-python main.py --exp_name coach_bert_encoder  --exp_id atp --tgt_domain AddToPlaylist --model_type train --device cuda:0
+python main.py \
+--exp_name coach_bert_encoder  \
+--exp_id atp \
+--tgt_domain AddToPlaylist \
+--model_type train \
+--device cuda:0 \
+--dump_path /home/sh/data/experiments \
+--bert_path /home/sh/bert-base-uncased \
+--file_path /home/sh/data/coachdata/snips
